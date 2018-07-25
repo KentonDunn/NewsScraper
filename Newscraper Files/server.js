442de3ef2434d3ad554c012638e69beb4d5487ea
KentonDunn <kenton.dunn@gmail.com>
2018-07-25 19:47:00 -0400

    mongolab to practice deploying

--- Newscraper Files/server.js
+++ Newscraper Files/server.js
@@ -23,8 +23,15 @@ var PORT = 3000 || process.env.PORT;
 // db.on("error", function (error) {
 //     console.log("Database Error:", error)
 // });
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
 
-mongoose.connect("mongodb://localhost/scraper");
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+//mongoose.connect("mongodb://localhost/scraper");
 
 
 console.log("\n******************\n" +
